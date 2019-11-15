1c812d8054d8816a951fa90511b51cda98942814
Adem Ali Durmuş <ademalidrms@gmail.com>
2019-11-15 23:53:19 +0300

    Currency exchange rate to Turkish Lira

--- src/App/CurrencyConverter.php
+++ src/App/CurrencyConverter.php
@@ -1,13 +1,14 @@
 <?php namespace AAD\TelegramBots\App;
 
 use AAD\TelegramBots\Helper\Config;
+use Respect\Validation\Validator as v;
 
 class CurrencyConverter
 {
     private $token;
 
     private static $routes = [
-        ['GET', '/currency/{token}/init', 'init'],
+        ['POST', '/currency/{token}/init', 'init'],
     ];
 
     public static function getRoutes()
@@ -17,13 +18,111 @@ class CurrencyConverter
 
     public function __construct($request, $args)
     {
-        $this->token = Config::get('bot_tokens')->currency_converter;
-        if ($args['token'] !== $this->token) {
+        $this->token = Config::get('bots')->currency['token'];
+        if ($args['token'] !== sha1($this->token)) {
             return header('HTTP/1.0 403 Permission Denied');
         }
     }
 
     public function init($request, $args)
     {
+        $url = "https://api.telegram.org/bot{$this->token}";
+        $data = file_get_contents('php://input');
+        $details = json_decode($data, true);
+        $chat_id = $details['message']['chat']['id'];
+        
+        if (!v::numeric()->validate($chat_id)) {
+            return header('HTTP/1.0 406 Not Acceptable');
+        }
+
+        $message = self::prepareResponse($details);
+        file_get_contents("{$url}/sendMessage?chat_id={$chat_id}&text={$message}");
+    }
+
+    public static function prepareResponse($details)
+    {
+        $username = strtolower(Config::get('bots')->currency['username']);
+        $message = $details['message']['text'];
+        $return = '';
+        
+        switch (strtolower($message)) {
+            case "/dolar":
+            case "/dolar@{$username}":
+            case "dolar":
+            case "usd":
+            case "amerikan doları":
+            case "abd doları":
+                $currency_details = self::getCurrencyDetails('USD');
+                $return = self::calculate(1, $currency_details);
+                break;
+                
+            case "/euro":
+            case "/euro@{$username}":
+            case "euro":
+            case "avro":
+                $currency_details = self::getCurrencyDetails('EUR');
+                $return = self::calculate(1, $currency_details);
+                break;
+                
+            case "/start":
+            case "/start@{$username}":
+                $return = "Merhaba {$details['message']['from']['first_name']},\nDoviz kuru hesaplama aracına hoş geldin.";
+                break;
+        }
+        
+        if (!v::notEmpty()->validate($return)) {
+            switch (true) {
+                case stristr($message, ' dolar'):
+                    $item = explode(' dolar', $message, 2);
+                    $currency_details = self::getCurrencyDetails('USD');
+                    $return = self::calculate($item[0], $currency_details);
+                    break;
+                    
+                case stristr($message, ' euro'):
+                    $item = explode(' euro', $message, 2);
+                    $currency_details = self::getCurrencyDetails('EUR');
+                    $return = self::calculate((float) $item[0], $currency_details);
+                    break;
+                
+                default:
+                    $item = explode(' ', $message, 2);
+                    $currency_details = self::getCurrencyDetails(strtoupper($item[1]));
+                    $return = self::calculate((float) $item[0], $currency_details);
+                    break;
+            }
+        }
+        
+        return urlencode($return);
+    }
+
+    public static function calculate($count, $currency_details)
+    {
+        if (!isset($currency_details['BanknoteBuying'])) {
+            return 'Komut anlaşılamadı';
+        }
+        
+        $buying = $currency_details['BanknoteBuying'];
+        $selling = $currency_details['BanknoteSelling'];
+        
+        if ($count > 0) {
+            $buying *= $count;
+            $selling *= $count;
+        } else {
+            $count = 1;
+        }
+        
+        return "{$count} {$currency_details['Isim']}\nALIŞ: {$buying}\nSATIŞ: {$selling}";
+    }
+
+    public static function getCurrencyDetails($currency_code = 'USD')
+    {
+        $return = [];
+        $data = simplexml_load_file('https://www.tcmb.gov.tr/kurlar/today.xml');
+        foreach ($data->Currency ?? [] as $key => $val) {
+            if (isset($val->attributes()['CurrencyCode']) && $val->attributes()['CurrencyCode'] == $currency_code) {
+                $return = (array) $val;
+            }
+        }
+        return $return;
     }
 }
